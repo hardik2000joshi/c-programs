e1e078be2ea48bc3cfde126b4fbb86ff6cab0074
hardik2000joshi <165023667+hardik2000joshi@users.noreply.github.com>
2024-12-01 11:27:03 +0530

    ios stream file added in C++

---
+++ iosStream.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // <iostream> - input output stream
+// input stream- to read data from source 
+// output stream- to write data to destination
+using namespace std;
+int main(){
+    cout<<"Hello C++ stream class";
+}
