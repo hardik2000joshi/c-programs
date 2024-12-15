7b9b57bc2f2d0ca3bf855e1afe7223864fc6eccd
hardik2000joshi <165023667+hardik2000joshi@users.noreply.github.com>
2024-12-15 21:14:15 +0530

    Character Array in C++

---
+++ Char_Array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char str[]={'a', 'b', 'c', 'd', '\0'};
+    int array[]={1, 2, 3, 4};
+    cout<<str<<endl;
+    cout<<array<<endl;
+    /*cout<<strlen(str)<<endl; //constant pointers*/
+    return 0;
+}
